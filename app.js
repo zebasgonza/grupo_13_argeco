58108acbc3b94c6145621b5a8e9803c53172164b
Fer9595 <omarfmaldonado95@gmail.com>
2023-05-13 00:06:03 -0300

    agregue get

--- app.js
+++ app.js
@@ -20,4 +20,12 @@ app.get("/productCard",function (req, res) {
     res.sendFile(rutaAlArchivo);
 });
 
+app.get("/register",function (req, res) {
+    console.log("entraron a la ruta register")
+
+    const rutaAlArchivo = path.join(__dirname, './views/register.html')
+
+    res.sendFile(rutaAlArchivo);
+});
+
 app.listen(3060,() => console.log("servidor corriendo en el puerto 3060"));
