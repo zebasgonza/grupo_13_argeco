2f63efbb2abe4b6fde84e44451cc51481aa47695
“zebasgonza” <sebasgonza45@gmail.com>
2023-05-22 01:22:00 -0500

    indicando en el entry point el uso de EJS

--- app.js
+++ app.js
@@ -3,17 +3,11 @@ const app = express();
 const path = require('path');
 const mainRoutes = require('./routes/mainRoutes');
 
+app.set("view engine", "ejs")
+
+
 app.use(express.static('public'));
 
 app.use(mainRoutes);
-
-
-// app.get("/productDetail",function (req, res) {
-//     console.log("entraron a la ruta Carrito de compras")
-
-//     const rutaAlArchivo = path.join(__dirname, './views/productDetail.html')
-
-//     res.sendFile(rutaAlArchivo);
-// });
 
 app.listen(3060,() => console.log("servidor corriendo en el puerto 3060"));
