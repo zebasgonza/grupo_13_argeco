79257b981982a2a17c47664dfacb82722e7f1bad
unknown <mawensypamplona@gmail.com>
2023-05-22 17:02:08 -0500

    Agregando funcionalidad al boton de Home

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,22 +1,16 @@
 const path = require('path');
 const controllers = {
     getIndex: (req, res) => {
-
         res.sendFile(path.join(__dirname, '../views/index.html'));
-        
-
     },
     getRegister: (req, res) => {
-
         res.sendFile(path.join(__dirname, '../views/register.html'));
-        
-
     },
     getDetail: (req, res) => {
-
         res.sendFile(path.join(__dirname, '../views/productDetail.html'));
-        
-
+    },
+    getProductCard: (req, res) => {
+        res.sendFile(path.join(__dirname, '../views/productCard.html'));
     },
 };
 module.exports = controllers;
