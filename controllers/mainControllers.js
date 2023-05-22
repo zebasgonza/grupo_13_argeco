95bdbdfdc75b87ab654209955c069287e267b3f5
Fer9595 <omarfmaldonado95@gmail.com>
2023-05-21 23:24:18 -0300

    ruta de vistas de seba

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -5,6 +5,18 @@ const controllers = {
         res.sendFile(path.join(__dirname, '../views/index.html'));
         
 
-    }
+    },
+    getRegister: (req, res) => {
+
+        res.sendFile(path.join(__dirname, '../views/register.html'));
+        
+
+    },
+    getDetail: (req, res) => {
+
+        res.sendFile(path.join(__dirname, '../views/productDetail.html'));
+        
+
+    },
 };
 module.exports = controllers;
