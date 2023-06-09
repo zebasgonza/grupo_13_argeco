1dd4ad18c40b3696a4674f65e0b0fef49bac0abd
“zebasgonza” <sebasgonza45@gmail.com>
2023-06-09 17:26:15 -0500

    agrego las rutas por metodos http, multer y requiero los productsControllers  junto con su explicación

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,7 +8,30 @@ const multer = require("multer");
 const productsControllers = require('../controllers/productsControllers');
 
 
-//metodos http / la ruta desde el url
+//metodo http /la ruta desde el url
 
-//@get /products
+//products (GET) nos MUESTRA el listado de productos
 router.get("../products",productsControllers.getproducts);
+
+// products/create (GET) nos MUESTRA la vista para crear un producto
+router.get('/create', productControllers.getCreate);
+
+// products (POST) permite que el usuario pueda CREAR y ENVIAR la info al servidor
+router.post('/', upload.any('img'), productControllers.postProducts);
+
+// products/:id/detail (GET) nos MUESTRA la vista del detalle de un producto en especifico de acuerdo a su ID
+router.get('/:id/detail', productControllers.getProductsDetail);
+
+//products/:id/delete (DELETE) ELIMINA información del detalle de un producto en especifico de acuerdo a su ID
+router.delete('/:id/delete', productControllers.deleteProducts);
+
+//products/:id/update (GET) nos MUESTRA la vista para editar un producto ya existente de acuerdo a su ID
+router.get('/:id/update', productControllers.getUpdate);
+
+// @PUT /products/:id/update permite reemplazar un dato ya existente de un producto según su ID
+router.put('/:id/update', productControllers.updateProducts);
+
+// products/cart (GET) MUESTRA la vista del carrito de compras
+router.get('/cart', productControllers.getCart);
+
+module.exports = route;
