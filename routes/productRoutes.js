3c4c1045c732380208b6ad9bd1a6a646759f5c33
“zebasgonza” <sebasgonza45@gmail.com>
2023-06-09 16:31:35 -0500

    agregando explicaciones de los pasos realizados

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,8 +2,13 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
+//requerimos multer para que el usuario pueda subir archivos.
 const multer = require("multer");
+//requerimos para llamar a los controladores de products
+const productsControllers = require('../controllers/productsControllers');
+
+
+//metodos http / la ruta desde el url
 
 //@get /products
-
 router.get("../products",productsControllers.getproducts);
