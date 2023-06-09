5b5c9980dc31def622013517244bf0520cfc3a04
“zebasgonza” <sebasgonza45@gmail.com>
2023-06-09 18:26:45 -0500

    agrego la configuración de router, middlewares y declaro requiero las constantes de rutas y métodos http adicionales

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 
 const path = require('path');
 const mainRoutes = require('./routes/mainRoutes');
+const productRoutes = require('./routes/productRoutes');
 
+
+/* ---- Configuración vistas EJS ---- */
 app.set("view engine", "ejs")
 
 app.set('views', [
     path.join(__dirname, './views/main'),
     path.join(__dirname, './views/products'),
     path.join(__dirname, './views/users'),
-    
-
 ]);
 
+/* ---- Middlewares ---- */
 app.use(express.static('public'));
+// nos permite recibir la info desde formularios a través del control
+app.use(express.urlencoded ({extended: true}));
+// convierte la información del json en un objeto de javascript para ser usar en el server.
+app.use(express.json)
+// habilita el uso de métodos http como (delete) y (put)
+app.use(methodOverride('_method'));
 
+/* ---- Routers ---- */
 app.use(mainRoutes);
+app.use('/products', productRoutes);
 
 app.listen(3060,() => console.log("servidor corriendo en el puerto http://localhost:3060"));
 
