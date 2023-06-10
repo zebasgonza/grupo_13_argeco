7d55f97b19e204c6bee15e7ef792f0bd5bf160ed
“zebasgonza” <sebasgonza45@gmail.com>
2023-06-09 21:28:27 -0500

    requiero path el models de products, y genero la constante controllers

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,3 +1,21 @@
 
 const path = require("path");
 
+const productModel = require('../models/product');
+
+const controllers = {
+    
+    // get /products 
+    getProducts: (req, res) => {
+        const productos = productModel.findAll();
+        res.render('productList', {
+            title: 'Productos',
+            productos
+        });
+    },
+
+
+
+}
+
+module.exports = controllers;
