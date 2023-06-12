e29a64cfa50e779c9be498d3347ee3a06857d9e4
unknown <mawensypamplona@gmail.com>
2023-06-12 16:50:52 -0500

    Se agregan products controllers

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -4,7 +4,7 @@ const path = require("path");
 const productModel = require('../models/products');
 
 const controllers = {
-    
+
     // get /products 
     getProducts: (req, res) => {
         const productos = productModel.findAll();
@@ -12,10 +12,33 @@ const controllers = {
             title: 'Productos',
             productos
         });
+
+    },
+    //get/products/:id/detail
+
+    getProductDetail: (req, res) => {
+
+        const id = number(req.params.id);
+
+        const productoAMostrar = productModel.findById(id);
+
+        if (!productoAMostrar) {
+            return res.send('error de id');
+        }
+
+        res.render('productDetail', { title: 'Detalle', product: productoAMostrar });
     },
 
+    // post/products
 
+    postProduct: (req, res) => {
 
+        let datos = req.body;
+
+        productModel.createOne(datos);
+
+        res.redirect('/');
+    },
 }
 
 module.exports = controllers;
