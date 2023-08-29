c954521a6ef7bfd0f3fe36cc249804c9203ca61b
unknown <mawensypamplona@gmail.com>
2023-08-28 20:36:45 -0500

    Se agrega BD tabla categoria_usuarios y se agrega modelo carrito

---
+++ database/models/carrito.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, DataTypes) => {
+    const alias = 'Carrito';
+
+    const cols = {
+        id_carrito: {
+            type: DataTypes.SMALLINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        id_usuario: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        fecha_inicio: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        
+        estado_pedido:{
+            type: DataTypes.LONGTEXT,
+            allowNull: false
+        },
+        fecha_finalizacion: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        precio:{
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+    }
+
+
+const config = {
+    tableName: 'carrito',
+    timestamps: false
+}
+
+const Carrito = sequelize.define (alias, cols,config)
+
+Carrito.associate = (models) =>{
+//Aca hacemos las relaciones
+} 
+
+return Carrito;
+}
+
+
