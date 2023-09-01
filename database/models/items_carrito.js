f9d33dac24daad3be5d553b6c5e13179be806d70
RosaMina0621 <skymina398@gmail.com>
2023-08-31 20:16:33 -0500

    agregando items_carrito

---
+++ database/models/items_carrito.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+    const alias = 'producto';
+
+    const cols = {
+        id_carrito: {
+            type: DataTypes.SMALLINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        id_producto: {
+            type: DataTypes.SMALLINT,
+            allowNull: false
+        },
+        cantidad: {
+            type: DataTypes.INT,
+            allowNull: false
+        },
+        precio:{
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+    }
+
+
+const config = {
+    tableName: 'items_carrito',
+    timestamps: false
+}
+
+const producto = sequelize.define (alias, cols,config)
+
+Carrito.associate = (models) =>{
+//Aca hacemos las relaciones
+} 
+
+return producto;
+}
