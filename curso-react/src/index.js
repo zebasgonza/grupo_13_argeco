031c1b64fb4f5b60956fd9e851282d1ef5e93982
Mawensy Pamplona <mawensypamplona@gmail.com>
2023-09-24 22:58:57 -0500

    agregando componentes de react users y products

--- curso-react/src/index.js
+++ curso-react/src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TotalProductos from './componentes/TotalProductos';
+import TotalUsuarios from './componentes/TotalUsuarios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TotalUsuarios/>
+
+    <TotalProductos/>
+    
+    <App/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- routes/api/productsApiRoutes.js
+++ routes/api/productsApiRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
-const userApiController = require('../../controllers/api/userApiController');
 const productsApiController = require('../../controllers/api/productsApiController');
 
-router.get('/', userApiController.getUsers);
+router.get('/', productsApiController.getProducts);
 router.get('/:id', productsApiController.getProductsById);
 
 
